2cbbf3683fbcba378ecc8659a0c8bbdae3554810
zuher83 <zu83@outlook.fr>
2021-02-28 15:28:30 +0100

    [Fix] Cleanup CSS and Html

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,7 +1,38 @@
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+$(document).on('click', '[data-toggle="lightbox"]', function (event) {
     event.preventDefault();
     $(this).ekkoLightbox({
         alwaysShowClose: true,
         showArrows: true,
     });
 });
+
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > 20) {
+//             $('#toTopBtn').fadeIn();
+//         } else {
+//             $('#toTopBtn').fadeOut();
+//         }
+//     });
+
+//     $('#toTopBtn').click(function () {
+//         $("html, body").animate({
+//             scrollTop: 0
+//         }, 1000);
+//         return false;
+//     });
+// });
+
+
+// $(document).on('click', '#toTopBtn', function(event) {
+
+//     var target = $( $(this).attr('href') );
+
+//     if( target.length ) {
+//         event.preventDefault();
+//         $('html, body').animate({
+//             scrollTop: target.offset().top
+//         }, 500);
+//     }
+
+// });
--- includes/form_1.php
+++ includes/form_1.php
@@ -1,24 +1,31 @@
-<?php	
+<?php
 	if(empty($_POST['name']) && strlen($_POST['name']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['input_504']) && strlen($_POST['input_504']) == 0 || empty($_POST['message']) && strlen($_POST['message']) == 0)
 	{
 		return false;
 	}
-	
+
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$input_504 = $_POST['input_504'];
 	$message = $_POST['message'];
-	
+
 	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
 
-	// Create email	
+	// Create email
 	$email_subject = "Message from your website";
 	$email_body = "You have received a new message. \n\n".
 				  "Name: $name \nEmail: $email \nInput_504: $input_504 \nMessage: $message \n";
-	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
+	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";
 	$headers .= "From: contact@yoursite.com\n";
-	$headers .= "Reply-To: $input_504";	
-	
+	$headers .= "Reply-To: $input_504";
+
+	if (!$_POST['name']) {
+		$errName = 'Please enter your name';
+	}
+
+	if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+		$errEmail = 'Please enter a valid email address';
+	}
+
 	mail($to,$email_subject,$email_body,$headers); // Post message
-	return true;			
-?>
+	return true;
