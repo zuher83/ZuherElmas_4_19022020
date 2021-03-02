38d9161a68d3edfd20e43a596a6de00bcd6cc6c7
zuher83 <zu83@outlook.fr>
2021-03-02 11:33:59 +0100

    Final project

--- assets/js/style.js
+++ assets/js/style.js
@@ -5,34 +5,3 @@ $(document).on('click', '[data-toggle="lightbox"]', function (event) {
         showArrows: true,
     });
 });
-
-// $(document).ready(function () {
-//     $(window).scroll(function () {
-//         if ($(this).scrollTop() > 20) {
-//             $('#toTopBtn').fadeIn();
-//         } else {
-//             $('#toTopBtn').fadeOut();
-//         }
-//     });
-
-//     $('#toTopBtn').click(function () {
-//         $("html, body").animate({
-//             scrollTop: 0
-//         }, 1000);
-//         return false;
-//     });
-// });
-
-
-// $(document).on('click', '#toTopBtn', function(event) {
-
-//     var target = $( $(this).attr('href') );
-
-//     if( target.length ) {
-//         event.preventDefault();
-//         $('html, body').animate({
-//             scrollTop: target.offset().top
-//         }, 500);
-//     }
-
-// });
